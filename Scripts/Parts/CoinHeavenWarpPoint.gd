f3546da4596d231f685e661dac3c2744eac7a1ec
KirbyKidJ <70983335+KirbyKid256@users.noreply.github.com>
2025-09-19 14:22:53 -0700

    Coin Heaven Warps Player to The Right Spot
    Tweaks the code a bit for the Coin Heaven Warping so the Player isn't sent back to the beginning and to where the exit is

--- Scripts/Parts/CoinHeavenWarpPoint.gd
+++ Scripts/Parts/CoinHeavenWarpPoint.gd
@@ -5,9 +5,10 @@ extends Node2D
 
 func _ready() -> void:
 	Level.vine_warp_level = heaven_scene
-	Level.in_vine_level = false
-	if Level.in_vine_level and PipeArea.exiting_pipe_id == -1:
-		for i in get_tree().get_nodes_in_group("Players"):
-			i.global_position = global_position
-			i.reset_physics_interpolation()
-			i.recenter_camera()
+	if Level.in_vine_level:
+		Level.in_vine_level = false
+		if PipeArea.exiting_pipe_id == -1:
+			for i in get_tree().get_nodes_in_group("Players"):
+				i.global_position = global_position
+				i.reset_physics_interpolation()
+				i.recenter_camera()
