5edc5466b8ebbc0d28d3b738d93284c86903a956
JHDev2006 <howelljoe2@gmail.com>
2025-09-24 17:53:33 +0100

    fixed a door bug with cusotm levels

--- Scripts/Parts/LevelTransition.gd
+++ Scripts/Parts/LevelTransition.gd
@@ -43,6 +43,8 @@ func _ready() -> void:
 	Global.p_switch_timer = -1
 	if Checkpoint.passed:
 		Door.unlocked_doors = Checkpoint.unlocked_doors.duplicate()
+	else:
+		Door.unlocked_doors = []
 	if Global.current_campaign == "SMBANN":
 		DiscoLevel.reset_values()
 	DiscoLevel.first_load = true
