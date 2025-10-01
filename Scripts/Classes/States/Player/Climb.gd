b8d85d1bb2ea26ddb45b91bb5a70311397b51ab9
JHDev2006 <howelljoe2@gmail.com>
2025-10-01 14:18:34 +0100

    adjusted player climbing position

--- Scripts/Classes/States/Player/Climb.gd
+++ Scripts/Classes/States/Player/Climb.gd
@@ -19,7 +19,7 @@ func physics_update(_delta: float) -> void:
 	if player.input_direction != 0 and climb_direction == 0 and not cutscene:
 		player.direction = -player.input_direction
 	player.sprite.scale.x = player.direction
-	player.global_position.x = vine.global_position.x - (8 * player.direction)
+	player.global_position.x = vine.global_position.x - (6 * player.direction)
 	if not cutscene and not auto_climb:
 		climb_direction = sign(Input.get_axis("move_up" + "_" + str(player.player_id),"move_down" + "_" + str(player.player_id)))
 	if vine.can_tele and player.global_position.y - 64 < vine.top_point and climb_direction == -1:
